7dcb54e9c23f037ae7c5b3648b38c5122d3e237e
Jiani <chenjiani930522@gmail.com>
2021-11-15 11:24:45 -0500

    update nextstrian

--- data_collection/generate_color_code.R
+++ data_collection/generate_color_code.R
@@ -3,3 +3,11 @@
 
 barplot(rep(1,7), col = rainbow(7))
 rainbow(7)
+
+nickColors <- function(n, h = c(120,400), l = c(.40,.70), s = c(.8,1), alpha = 1){
+  require(colorspace)
+  require(scales)
+  return (alpha(hex(HLS(seq(h[1],h[2],length.out = n), seq(l[1],l[2],length.out = n), seq(s[1],s[2],length.out=n))), alpha))
+}
+barplot(rep(1,7), col = nickColors(7))
+nickColors(7)
